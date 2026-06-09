3ed23769db723920aea72c836fc0f638df25d2d1
ByronDev03 <byronjorge.158@gmail.com>
2026-06-08 23:29:59 -0600

    style: add animation in mobile

--- main.js
+++ main.js
@@ -1,50 +1,3 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
-function seleccionar(link) {
-    let opciones = document.querySelectorAll('#links  a');
-
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    link.className = "seleccionado";
-
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
-    let x = document.getElementById("nav");
-    
-    x.classList.remove("responsive");
-    x.classList.remove("active");
-}
-
-//función que muestra el menu responsive
-function responsiveMenu() {
-    let x = document.getElementById("nav");
-    
-    x.classList.toggle("responsive");
-    x.classList.toggle("active");
-}
-
-const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-    reveals.forEach((element) => {
-        const windowHeight = window.innerHeight;
-        const revealTop = element.getBoundingClientRect().top;
-
-        if (revealTop < windowHeight - 100) {
-            element.classList.add("active");
-        }
-    });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-
-revealOnScroll();
-
-
-
-
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
@@ -93,3 +46,23 @@ footerAccordions.forEach(item => {
   });
 
 });
+
+
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach((element) => {
+        const windowHeight = window.innerHeight;
+        const revealTop = element.getBoundingClientRect().top;
+
+        if (revealTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
